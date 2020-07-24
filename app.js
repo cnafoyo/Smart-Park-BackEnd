8d59b2f48b3b40206d44f0509935a45c9c1cc218
LincolnLidanya <lincolnlidanya@gmail.com>
2020-07-24 12:48:41 +0300

    Add port changes

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ app.use('/packingspot',packingspotRouter)
 
 module.exports = app;
 
+/*
 app.listen(3045, ()=>{
     console.log("Server started");
 })
-
+*/
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
